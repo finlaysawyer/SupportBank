b90db6cad456aeb00b84146622b920ad0518b110
Finlay <finjsawyer@gmail.com>
2020-10-14 19:50:01 +0100

    Refactor & cleanup

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,12 +1,14 @@
 package training.supportbank;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class Main {
     public static void main(String args[]) {
@@ -20,11 +22,11 @@ public class Main {
                     System.out.println("Listing all records:");
 
                     List<Account> accountList = importCSVAccounts();
-            
+
                     for (Account acc : accountList) {
                         System.out.println(acc.getName() + ": " + new DecimalFormat("#.##").format(acc.getBalance()));
                     }
-        
+
                 } else if (command.length >= 3) {
                     String nameTarget = String.join(" ", sliceArray(command, 1, 3));
                     System.out.println("Listing transactions for " + nameTarget + ":");
@@ -45,7 +47,7 @@ public class Main {
     public static List<Account> importCSVAccounts() {
         CSVReader records = null;
         List<Account> accountList = new ArrayList<Account>();
-        
+
         try {
             records = new CSVReader(new FileReader("D:\\dev_corndel\\SupportBank\\Transactions2014.csv"));
             String[] record = null;
@@ -58,7 +60,7 @@ public class Main {
                 Transaction transaction = new Transaction(record[2], record[1], record[0], record[3], Float.parseFloat(record[4]));
                 Account nameToAccount = null;
                 Account nameFromAccount = null;
-                
+
                 if (!accountList.isEmpty()) {
                     for (int i = 0; i < accountList.size(); i++) {
                         if (accountList.get(i).getName().equals(nameTo)) { // If account already exists
@@ -70,36 +72,37 @@ public class Main {
                     }
 
                     if (nameToAccount == null) {
-                        Account accountTo = new Account(nameTo, balance);
-                        accountList.add(accountTo);
-                        accountTo.addTransaction(transaction);
+                        nameToAccount = new Account(nameTo, balance);
+                        accountList.add(nameToAccount);
+                        nameToAccount.addTransaction(transaction);
                     } else {
                         nameToAccount.addToBalance(balance);
                         nameToAccount.addTransaction(transaction);
                     }
 
                     if (nameFromAccount == null) {
-                        Account accountFrom = new Account(nameFrom, -1 * balance);
-                        accountList.add(accountFrom);
-                        accountFrom.addTransaction(transaction);
+                        nameFromAccount = new Account(nameFrom, -1 * balance);
+                        accountList.add(nameFromAccount);
+                        nameFromAccount.addTransaction(transaction);
                     } else {
                         nameFromAccount.removeFromBalance(balance);
                         nameFromAccount.addTransaction(transaction);
-                        
                     }
+                    
                 } else {
-                    Account accountTo = new Account(nameTo, balance);
-                    accountList.add(accountTo);
-                    accountTo.addTransaction(transaction);
-                    Account accountFrom = new Account(nameFrom, -1 * balance);
-                    accountList.add(accountFrom);
-                    accountFrom.addTransaction(transaction);
+                    nameToAccount = new Account(nameTo, balance);
+                    accountList.add(nameToAccount);
+                    nameToAccount.addTransaction(transaction);
+
+                    nameFromAccount = new Account(nameFrom, -1 * balance);
+                    accountList.add(nameFromAccount);
+                    nameFromAccount.addTransaction(transaction);
                 }
             }
             records.close();
 
-        } catch (Exception e) {
-            System.out.println("Couldn't find file or encountered an error: " + e);
+        } catch (IOException | NumberFormatException | CsvValidationException e) {
+            System.out.println("Couldn't find file or encountered a formatting exception: " + e);
         }
         return accountList;
     }
